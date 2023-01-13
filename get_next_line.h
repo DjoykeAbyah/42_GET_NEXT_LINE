9155619591128e56fe1c9d439d9feb497475fe0d
DjoykeReijans <crudendjoyke@gmail.com>
2023-01-13 23:38:22 +0100

    header changed main changed2

--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,12 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/22 13:18:23 by dreijans      #+#    #+#                 */
-/*   Updated: 2023/01/13 20:06:08 by dreijans      ########   odam.nl         */
+/*   Updated: 2023/01/13 23:37:59 by djoyke        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# include <unistd.h>
-# include <fcntl.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <stddef.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 100000
